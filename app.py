af150a6e95ba38f715e0e3dd2d8fd6affac0e548
MatSim91 <mateus.r.simoes@gmail.com>
2021-06-04 12:54:00 +0000

    Add signup page and form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -28,6 +29,11 @@ def home():
 def scientists():
     scientists = mongo.db.scientists.find()
     return render_template("scientists.html", scientists=scientists)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
