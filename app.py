cab0cec32ea763844f66f1aaed3c8ab1787de849
MatSim91 <mateus.r.simoes@gmail.com>
2021-06-04 14:08:41 +0000

    Add and customize Sign up fuctionality

--- app.py
+++ app.py
@@ -31,8 +31,25 @@ def scientists():
     return render_template("scientists.html", scientists=scientists)
 
 
+# Registration functionaly created with the help of the Code Institute Mini Project
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This User already exists")
+            return redirect(url_for("signup"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("signup.html")
 
 
