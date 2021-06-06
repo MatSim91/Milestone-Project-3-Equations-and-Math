960465a4263ad8fd5802a2f56188910375615b5c
MatSim91 <mateus.r.simoes@gmail.com>
2021-06-06 17:14:15 +0000

    Update datepicker to proper year (1300 to 2000) and styled add scientist section

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
 
-/* jQuery for Mobile Navbar Initialization from Materialize  */
+/* jQuery initialization for features from Materialize  */
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('textarea#description').characterCounter();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+      format: 'dd mmmm, yyyy',
+      yearRange: [1300, 2000],
+      showClearBtn: true,
+      i18n: {
+        done: 'Select'
+      }
+    });
   });
