b296bf67e3b422e0c4b23e1557378fe62ddc8a03
MatSim91 <mateus.r.simoes@gmail.com>
2021-06-12 11:47:17 +0000

    Update margin on search and reset buttons

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/add_scientist", methods=["GET", "POST"])
+@app.route("/scientist/add", methods=["GET", "POST"])
 def add_scientist():
     if request.method == "POST":
         scientist = {
