a30f893255459f0cf58a3acb0d54b63d2706911a
MatSim91 <mateus.r.simoes@gmail.com>
2021-06-17 10:05:00 +0000

    Fix PEP8 compliant error

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def login():
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                    existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for("home"))
